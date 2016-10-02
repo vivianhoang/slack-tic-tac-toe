4681b4360e32a52580ae95a4c75d3984ec619819
Vivian Hoang <VivianH@Vivians-MacBook-Pro.local>
2016-10-02 10:41:10 -0700

    Fixed request for user_name

--- test-server.py
+++ test-server.py
@@ -60,7 +60,7 @@ def board():
 
 @app.route('/move', methods=["POST"])
 def move():
-    person = request.form.get(user_name)
+    person = request.form.get('user_name')
     if current_user == person:
         position = 'hello'
         inputPosition = request.form.get('text')
