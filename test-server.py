9968ff268a38741c2afd1b41f6fd220ad8bc1bb2
VivH <vivtsehoang@gmail.com>
2016-10-02 07:25:03 +0000

    Created test file for tic tac toe.

---
+++ test-server.py
@@ -0,0 +1,60 @@
+# from flask import Flask, requests
+# from flask_restful import Resource, Api
+
+# app = Flask(__name__)
+# api = Api(app)
+
+# todos = {}
+
+# class TodoSimple(Resource):
+#     def get(self, todo_id):
+#         return {todo_id: todos[todo_id]}
+
+#     def put(self, todo_id):
+#         todos[todo_id] = request.form['data']
+#         return {todo_id: todos[todo_id]}
+
+# api.add_resource(TodoSimple, '/<string:todo_id>')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.secret_key = "ABC123"  # For demo only.
+entries: {
+    topLeft: "O",
+    bottomRight: "X",
+}
+
+entryPositionNames = {
+    'top-left': True,
+    'top-middle': True,
+    'top-right': True,
+    'middle-left': True,
+    'middle': True,
+    'middle-right': True,
+    'bottom-left': True,
+    'bottom-middle': True,
+    'bottom-right': True,
+}
+
+def verifyPosition(position):
+    if position in entries:
+
+
+
+@app.route('/board')
+def board():
+
+    return "[.][.][.]<br>[.][.][.]<br>[.][.][.]"
+
+@app.route('/x')
+def x():
+    return "you have an x"
+
+if __name__ == "__main__":
+    
+    app.run(host="0.0.0.0")
