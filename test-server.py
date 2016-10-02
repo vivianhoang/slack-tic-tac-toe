90f3f5139fb45eba667bf612467cacb6c2aee02c
Vivian Hoang <VivianH@Vivians-MacBook-Pro.local>
2016-10-02 14:25:51 -0700

    Looking for available moves.

--- test-server.py
+++ test-server.py
@@ -134,7 +134,7 @@ def move():
     print person_submitted
     print current
 
-    if current == person_submitted:
+    if current == ('@' + person_submitted):
         position = 'hello'
         inputPosition = request.form.get('text')
         if inputPosition:
