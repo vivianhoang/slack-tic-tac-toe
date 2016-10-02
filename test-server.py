845b280dd9bc05c6f19ca9ced2bf00ea4dcbf334
Vivian Hoang <VivianH@Vivians-MacBook-Pro.local>
2016-10-02 02:42:26 -0700

    Changed port.

--- test-server.py
+++ test-server.py
@@ -66,4 +66,4 @@ if __name__ == '__main__':
     DEBUG = "NO_DEBUG" not in os.environ
     PORT = int(os.environ.get("PORT", 5000))
 
-    app.run(host="0.0.0.0", port=33507, debug=DEBUG)
+    app.run(host="0.0.0.0", port=PORT, debug=DEBUG)
