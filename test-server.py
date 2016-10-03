134058574d6baed73a16cd0422ca77a63d3c4ee3
Vivian Hoang <VivianH@Vivians-MacBook-Pro.local>
2016-10-03 13:49:34 -0700

    Testing token on heroku.

--- test-server.py
+++ test-server.py
@@ -89,9 +89,8 @@ def state():
         #                 ]})
 
 
-        slack_client.api_call("chat.postMessage", channel=channel_name, text='lol', username='tic-tac-toe', icon_emoji=':robot_face:')
+        slack_client.api_call("chat.postMessage", channel=channel_id, text='lol', username='tic-tac-toe', icon_emoji=':robot_face:')
         return "It is cloudy"
-        print "It's pouring hard"
         # return send_message(channel_id, message)
         # return response.send({"response_type": "in_channel",
         #                       "text": "%s wants to play tic-tac-toe with %s." % (user_name, invited_player),
