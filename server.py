59eeb6a9e7539d81b6b5c8b2c8ef2427d2c71408
Vivian Hoang <VivianH@Vivians-MacBook-Pro.local>
2016-10-05 09:57:36 -0700

    Testing different channels to initialize game.

--- server.py
+++ server.py
@@ -47,8 +47,10 @@ def state():
 
     #channel['channel_id'] = {creator, inviter, invited, channel_id} PUT CURRENT STATE INSIDE
 
-    if channels.get('channel_id', newState).get("in_progress") == False:
+    if channels.get('channel_id', newState) not in channels.keys():
         channels[channel_id] = newState
+
+    if channels.get('channel_id').get("in_progress") == False:
         user_id = request.form.get('user_id')
         # needed to convert to string to prevent saving user_name as type_unicode
         user_name = str(request.form.get('user_name'))
